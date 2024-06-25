d85a60a5e3a1c09d8b4f82fbefac4cc7cf659fb6
Jennilop1 <jenniloponsol@gmail.com>
2024-06-25 13:09:21 +0300

    WelcomeBanner.tsx

--- src/sections/Dashboard/WelcomeBanner.tsx
+++ src/sections/Dashboard/WelcomeBanner.tsx
@@ -102,20 +102,20 @@ export function WelcomeBanner() {
   return (
     <Welcome>
       <div>
-        <h1>Welcome to Gamba v2 👋</h1>
+        <h1>Welcome to $JLO </h1>
         <p>
-          A fair, simple and decentralized casino on Solana.
+          A fair, simple and decentralized $JLO casino on Solana.
         </p>
       </div>
       <Buttons>
-        <button onClick={() => window.open('https://v2.gamba.so/', '_blank')}>
-          🚀 Add Liquidity
+        <button onClick={() => window.open('https://raydium.io/swap/?outputCurrency=2h8PLKnJzqYzwqakvHK53QC93PcRzfXGGNFPUA13WCtd&inputMint=2h8PLKnJzqYzwqakvHK53QC93PcRzfXGGNFPUA13WCtd&outputMint=sol')}>
+          🚀 BUY $JLO
         </button>
-        <button onClick={() => window.open('https://github.com/gamba-labs/gamba', '_blank')}>
-          👨‍💻 Build your own
+        <button onClick={() => window.open('https://x.com/jenniloponsol')}>
+          👨‍💻 Twitter
         </button>
-        <button onClick={() => window.open('https://discord.gg/HSTtFFwR', '_blank')}>
-          💬 Discord
+        <button onClick={() => window.open('https://x.com/jenniloponsol')}>
+          💬 Telegram
         </button>
       </Buttons>
     </Welcome>
